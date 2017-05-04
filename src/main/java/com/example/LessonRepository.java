0527e9fa961c9e3866d466eadb83854bfe61197a
Bittu Joju <bittujoju@gmail.com>
2017-05-03 19:19:51 -0600

    crud first exercise

---
+++ src/main/java/com/example/LessonRepository.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by trainer11 on 5/3/17.
+ */
+public interface LessonRepository extends CrudRepository<Lesson, Long> {
+
+
+}
---
+++ src/main/java/com/example/LessonsController.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by trainer11 on 5/3/17.
+ */
+@RestController
+@RequestMapping("/lessons")
+public class LessonsController {
+
+    private final LessonRepository repository;
+
+    public LessonsController(LessonRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("")
+    public Iterable<Lesson> all() {
+        return this.repository.findAll();
+    }
+
+    @PostMapping("")
+    public Lesson create(@RequestBody Lesson lesson) {
+        return this.repository.save(lesson);
+    }
+
+}
